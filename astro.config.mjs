986df891b8f39a35b3f1d271c55e293e832e2dbc
valenzy-wkkr <jvalenciamunoz808@gmail.com>
2025-11-11 08:59:08 -0500

    chore: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.SITE_URL,
+  base: process.env.BASE_PATH ?? '/',
   vite: {
     plugins: [tailwindcss()]
   }
